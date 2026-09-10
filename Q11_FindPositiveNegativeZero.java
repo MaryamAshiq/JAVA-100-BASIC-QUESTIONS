c3978494ee58ae3c72be7a6b6f623c8b51502b57
Maryam Ashiq <maryamashiq867@gmail.com>
2026-09-10 13:41:37 -0700

    Q11_FindPositiveNegativeZero.java

---
+++ Q11_FindPositiveNegativeZero.java
@@ -0,0 +1,12 @@
+public class Q11_FindPositiveNegativeZero {
+    public static void main(String[] args) {
+        int num = 0;
+        if (num > 0) {
+            System.out.println(num + " is positive.");
+        } else if (num < 0) {
+            System.out.println(num + " is negative.");
+        } else {
+            System.out.println(num + " is zero.");
+        }
+    }
+}
