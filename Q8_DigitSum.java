a69652256fdeef539af0fccdb2b78d632be6c448
Maryam Ashiq <maryamashiq867@gmail.com>
2026-09-10 02:20:59 -0700

    Q8_DigitSum.java

---
+++ Q8_DigitSum.java
@@ -0,0 +1,11 @@
+public class Q8_DigitSum {
+    public static void main(String[] args) {
+        int num = 12345;
+        int sum= 0;
+        while(num != 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        System.out.println("Sum of digits: " + sum);
+    }
+}
