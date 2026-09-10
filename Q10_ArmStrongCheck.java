8d37566e7434df342d896384b5a01828be692c39
Maryam Ashiq <maryamashiq867@gmail.com>
2026-09-10 02:39:08 -0700

    Q10_ArmStrongCheck.java

---
+++ Q10_ArmStrongCheck.java
@@ -0,0 +1,17 @@
+public class Q10_ArmStrongCheck {
+    public static void main(String[] args) {
+        int num = 153;
+        int originalNum = num;
+        int sum = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, 3);
+            num /= 10;
+        }
+        if (sum == originalNum) {
+            System.out.println(originalNum + " is an Armstrong number.");
+        } else {
+            System.out.println(originalNum + " is not an Armstrong number.");
+        }
+    }
+}
