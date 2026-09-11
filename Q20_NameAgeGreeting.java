84b50ef33d16c1fab8ede1fa050bbacc93d1f1dc
Maryam Ashiq <maryamashiq867@gmail.com>
2026-09-11 10:41:06 -0700

    Q20_NameAgeGreeting

---
+++ Q20_NameAgeGreeting.java
@@ -0,0 +1,7 @@
+public class Q20_NameAgeGreeting {
+    public static void main(String[] args) {
+        String name = "John";
+        int age = 25;
+        System.out.println("Hello, " + name + "! You are " + age + " years old.");
+    }
+}
