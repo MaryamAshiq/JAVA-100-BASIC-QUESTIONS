0288b75799e7f8019e250f778cb1a1557e97cf7e
Maryam Ashiq <maryamashiq867@gmail.com>
2026-09-12 03:03:14 -0700

    Q23_PercentageCalcultor

---
+++ Q23_PercentageCalculator.java
@@ -0,0 +1,8 @@
+public class Q23_PercentageCalculator {
+    public static void main(String[] args) {
+        int marksObtained = 450;
+        int totalMarks = 500;
+        double percentage = (marksObtained / totalMarks) * 100;
+        System.out.println("Percentage: " + percentage + "%");
+    }
+}
