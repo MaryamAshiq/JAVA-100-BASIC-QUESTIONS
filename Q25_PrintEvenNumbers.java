85640c2c2f3c77e7fb60989dc17e38a910cad425
Maryam Ashiq <maryamashiq867@gmail.com>
2026-09-13 04:24:49 -0700

    Q25_PrintEvenNumbers

---
+++ Q25_PrintEvenNumbers.java
@@ -0,0 +1,10 @@
+public class Q25_PrintEvenNumbers {
+    public static void main(String[] args) {
+        int Num = 20;
+        for (int i = 0; i <= Num; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
