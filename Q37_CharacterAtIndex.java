d10b9638f6ae039b0e9673d0b9dcedf93ffc644b
Maryam Ashiq <maryamashiq867@gmail.com>
2026-09-15 11:00:45 -0700

    Q37_CharacterAtIndex

---
+++ Q37_CharacterAtIndex.java
@@ -0,0 +1,8 @@
+public class Q37_CharacterAtIndex {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        int index = 4;
+        char character = str.charAt(index);
+        System.out.println("Character at index " + index + " is : " + character);
+    }
+}
