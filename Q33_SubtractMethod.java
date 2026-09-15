0dff264ec4921cca0a709c91b489a9a7006064e8
Maryam Ashiq <maryamashiq867@gmail.com>
2026-09-15 03:24:22 -0700

    Q33_SubtractMethod

---
+++ Q33_SubtractMethod.java
@@ -0,0 +1,11 @@
+public class Q33_SubtractMethod {
+    public static int subtractNumbers(int a, int b) {
+        return a - b;
+    }
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        int result = subtractNumbers(a, b);
+        System.out.println("Result: " + result);
+    }
+}
