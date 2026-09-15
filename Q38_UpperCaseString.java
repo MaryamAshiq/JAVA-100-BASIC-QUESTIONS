99ec4fc72b9e4ccc0edc1326c20469db98c9bb6a
Maryam Ashiq <maryamashiq867@gmail.com>
2026-09-15 11:36:31 -0700

    Q38_UpperCaseString

---
+++ Q38_UpperCaseString.java
@@ -0,0 +1,8 @@
+public class Q38_UpperCaseString {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String upperCaseStr = str.toUpperCase();
+        System.out.println("Original string: " + str);
+        System.out.println("Uppercase string: " + upperCaseStr);
+    }
+}
---
+++ Q39_LowerCase.java
@@ -0,0 +1,7 @@
+public class Q39_LowerCase {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String LowerCaseStr = str.toLowerCase();
+        System.out.println( LowerCaseStr);
+    }
+}
