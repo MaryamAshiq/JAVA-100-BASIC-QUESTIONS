0042ca32bbeaa30f78d0e319cb0a661b884834c9
Maryam Ashiq <maryamashiq867@gmail.com>
2026-09-15 01:55:10 -0700

    Q31_SumMethod

---
+++ Q31_SumMethod.java
@@ -0,0 +1,12 @@
+public class Q31_SumMethod {
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = addNumbers(a, b);
+        System.out.println("Sum: " + sum);
+    }
+}
