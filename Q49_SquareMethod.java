3bb51cc791e20f953dc76ee02bb97a11ff4ab5aa
Maryam Ashiq <maryamashiq867@gmail.com>
2026-09-18 01:52:51 -0700

    Q49_SquareMethod

---
+++ Q49_SquareMethod.java
@@ -0,0 +1,11 @@
+public class Q49_SquareMethod {
+     public static int SqaureOfNumber(int number) {
+      return number * number ;
+     }
+  public static void main(String[] args){
+      int number = 5;
+      int result = SqaureOfNumber(number);
+      System.out.println(result);
+  }
+}
+
