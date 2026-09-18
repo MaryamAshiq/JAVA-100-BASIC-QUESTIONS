b9640866eca7eaeb899560651c898824825f0988
Maryam Ashiq <maryamashiq867@gmail.com>
2026-09-18 00:54:19 -0700

    Q42_SimpleCalculator

---
+++ Q42_SimpleCalculator.java
@@ -0,0 +1,14 @@
+public class Q42_SimpleCalculator {
+       public static void main(String[] args) {
+      int a = 10;
+      int b = 20;
+     int sum = a + b;
+     int subtract = a - b;
+     int product = a * b;
+     int division = a / b;
+       System.out.println(sum);
+       System.out.println(subtract);
+       System.out.println(product);
+       System.out.println(division);
+   }
+}
