449b18517c27eb68f392affd13bc73c5d184e9de
Maryam Ashiq <maryamashiq867@gmail.com>
2026-09-18 00:59:23 -0700

    Q43_PrintNumbers

---
+++ Q43_PrintNumbers.java
@@ -0,0 +1,8 @@
+public class Q43_PrintNumbers {
+    public static void main(String [] args) {
+        int N = 50 ;
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
+    }
+}
