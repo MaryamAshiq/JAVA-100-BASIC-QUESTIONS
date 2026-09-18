cc04580e62a20b32fd4a31ac09603083b3f01d58
Maryam Ashiq <maryamashiq867@gmail.com>
2026-09-18 01:13:00 -0700

    Q45_ArraySum

--- Q44_EvenNumbers.java
+++ Q44_EvenNumbers.java
@@ -1,10 +1,8 @@
 public class Q44_EvenNumbers {
     public static void main(String [] args) {
         int N = 50;
-        for (int i = 2; i <= N; i++ ) {
-            if (i % 2 == 0) {
-                System.out.println(i);
-            }
+        for (int i = 2; i <= N; i += 2) {
+            System.out.println(i);
         }
     }
 }
---
+++ Q45_ArraySum.java
@@ -0,0 +1,11 @@
+public class Q45_ArraySum {
+    public static void main(String[] args) {
+        int[] num = new int[]{1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int i = 0; i < num.length; i++) {
+            sum = sum + num[i];
+
+        }
+        System.out.println(sum);
+    }
+}
